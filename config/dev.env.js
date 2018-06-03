cfa5525566c6d7b7b170db60815e3ebf4477a427
EvgeniyGordinskiy <evgeniy@gordinskiy.com>
2018-06-03 19:05:08 +0300

    Changed port and API_LOCATION

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-   API_LOCATION: '"http://work/"',
+   API_LOCATION: '"http://localhost:8000/api"',
 })
